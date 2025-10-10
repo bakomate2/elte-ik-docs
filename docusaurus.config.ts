9d19042a3dd51983ac3e86d3341407ea67f559a1
Bakonyi Máté <matebakonyi03@gmail.com>
2025-10-10 15:26:49 +0200

    fixed urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://elteik.bmate.dev',
+  url: 'https://bakomate2.github.io/elte-ik-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/elte-ik-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
