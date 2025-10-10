7b178384fbc7e5f0ba840d3788c158fb97ca5209
Bakonyi Máté <matebakonyi03@gmail.com>
2025-10-10 15:27:24 +0200

    fixed urls again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://bakomate2.github.io/elte-ik-docs/',
+  url: 'https://bakomate2.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/elte-ik-docs/',
