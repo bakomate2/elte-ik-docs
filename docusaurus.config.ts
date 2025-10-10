2b78fd4a6623c3e3f8db466b0c2103b5f4938f37
Bakonyi Máté <matebakonyi03@gmail.com>
2025-10-10 15:32:46 +0200

    added domain settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,18 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://bakomate2.github.io/',
+  url: 'https://elteik.bmate.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/elte-ik-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bakomate2', // Usually your GitHub org/user name.
   projectName: 'elte-ik-docs', // Usually your repo name.
+
+  // Trailing slash behavior for GitHub Pages
+  trailingSlash: false,
 
   onBrokenLinks: 'warn',
 
